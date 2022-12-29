6f4168e7214bb5f0cbb39eea1b3f1a8f8a08a0ec
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-12-28 19:08:35 -0700

    added filter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule
