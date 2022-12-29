eb693de66332af98e473d6153091e5cfeae0ca30
mustafasahin0 <sahinmustafaenes4@gmail.com>
2022-12-28 19:01:47 -0700

    added new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
